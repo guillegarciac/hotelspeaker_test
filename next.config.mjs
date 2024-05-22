2a1fdea39898e93a6e74dcfb3661920a83cc9b23
guille garcia <ggarciacardiel@gmail.com>
2024-05-22 15:39:13 +0200

    updated mjs and config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://external-api.com/:path*'
+        destination: 'https://external-api.com/:path*',
       },
     ];
   },
