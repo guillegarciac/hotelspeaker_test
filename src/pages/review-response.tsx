0982df847aacc57e8bab79ae008e24187753726b
guille garcia <ggarciacardiel@gmail.com>
2024-05-25 12:30:43 +0200

    reverts

--- src/pages/review-response.tsx
+++ src/pages/review-response.tsx
@@ -21,19 +21,16 @@ const ReviewResponse: React.FC = () => {
   const fetchData = async () => {
     const res = await fetch('/api/callback');
     if (res.ok) {
-      const rawData: string = await res.text(); // Get the raw response data as a string
-      const dataStartIndex = rawData.indexOf('{'); // Find the starting index of the JSON object
-      const jsonData = JSON.parse(rawData.substring(dataStartIndex)); // Extract and parse the JSON object
-      
-      console.log('Received response:', jsonData);
+      const data: Response = await res.json();
+      console.log('Received response:', data);
     
       // Check if the received review_id matches the one from the query parameter
       console.log('Review ID from query:', reviewId);
-      console.log('Review ID from response:', jsonData.review_id);
-      if (jsonData && reviewId && jsonData.review_id === reviewId) {
+      console.log('Review ID from response:', data.review_id);
+      if (data && reviewId && data.review_id === reviewId) {
         console.log('Review ID matches. Stopping polling.');
         setShouldPoll(false); // Stop polling if review_id matches
-        setResponse(jsonData); // Set the response in the state
+        setResponse(data); // Set the response in the state
         setLoading(false); // Set loading to false once data is fetched
       } else {
         console.log('Review ID does not match. Continuing polling.');
