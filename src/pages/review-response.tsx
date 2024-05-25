63aadb49b27330b3073d17053c6ab07e8a17bcd3
guille garcia <ggarciacardiel@gmail.com>
2024-05-25 11:24:11 +0200

    continue polling until matching reviewId

--- src/pages/review-response.tsx
+++ src/pages/review-response.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import styles from '../styles/ReviewResponse.module.css';
 
 interface Response {
@@ -11,32 +12,34 @@ interface Response {
 }
 
 const ReviewResponse: React.FC = () => {
+  const router = useRouter();
+  const { reviewId } = router.query; // Get reviewId from router query
   const [response, setResponse] = useState<Response | null>(null);
   const [loading, setLoading] = useState(true); // State variable to track loading state
-  const [shouldPoll, setShouldPoll] = useState(true); 
 
   const fetchData = async () => {
     const res = await fetch('/api/callback');
     if (res.ok) {
       const data: Response = await res.json();
       setResponse(data);
-      setShouldPoll(false);
-      setLoading(false); // Set loading to false once data is fetched
+
+      // Check if the received review_id matches the one from the query parameter
+      if (data && reviewId && data.review_id === reviewId) {
+        setLoading(false); // Stop loading if review_id matches
+      }
     }
   };
 
   useEffect(() => {
-    fetchData(); 
+    fetchData();
 
-    // Polling every 10 seconds if shouldPoll is true
-    if (shouldPoll) {
-      const pollingInterval = setInterval(fetchData, 10000);
+    // Polling every 10 seconds
+    const pollingInterval = setInterval(fetchData, 10000);
 
-      return () => {
-        clearInterval(pollingInterval);  
-      };
-    }
-  }, [shouldPoll]); 
+    return () => {
+      clearInterval(pollingInterval);
+    };
+  }, []); // Run once when the component mounts
 
   return (
     <div className={styles.container}>
